3e0a048db9a5d7c428729a3c5a92bdca740a9f8e
Hamelonov <leonhamelonovich@gmail.com>
2026-08-05 16:52:27 +0500

    Merge conflict fix

--- frontend/src/widgets/header/model/navLinks.ts
+++ frontend/src/widgets/header/model/navLinks.ts
@@ -9,16 +9,9 @@ import {
 import { AppRouter } from '@/shared/config/app-router';
 
 export const navLinks = [
-<<<<<<< HEAD
-  { label: 'Форум', href: AppRouter.threads },
-  { label: 'Участники', href: AppRouter.main },
-  { label: 'Блог', href: AppRouter.main },
-  { label: 'Правила', href: AppRouter.rules.root },
-=======
   { label: 'Главная', href: AppRouter.main, Icon: FaHome },
   { label: 'Форум', href: AppRouter.threads, Icon: FaBullhorn },
   { label: 'Участники', href: AppRouter.participants, Icon: FaUsers },
   { label: 'Блог', href: AppRouter.blog.root, Icon: FaFeather },
   { label: 'Правила', href: AppRouter.rules.root, Icon: FaBalanceScale },
->>>>>>> main
 ];

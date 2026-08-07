46991a067f367527c99485dfe1c375feee0baf08
Hamelonov <leonhamelonovich@gmail.com>
2026-08-07 20:46:08 +0500

    Добавлена картинка space-man.png. Исправлен импорт utils.css

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Montserrat } from 'next/font/google';
 import Providers from './providers/providers';
 
 import './styles/globals.css';
-import './utils.css';
+import './styles/utils.css';
 
 const montserrat = Montserrat({
   variable: '--font-main',

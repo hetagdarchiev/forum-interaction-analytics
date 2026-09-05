876a6c5b0ae797ac72ef2abd962b497ae1908f29
Nazar <nazarpalamar46@gmail.com>
2026-09-05 18:21:30 +0200

    fix build error

--- frontend/src/app/(main)/(pages)/(private)/threads/new/page.tsx
+++ frontend/src/app/(main)/(pages)/(private)/threads/new/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Suspense } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod/dist/zod.js';
 
@@ -37,20 +38,22 @@ export default function NewThreadPage() {
   });
   return (
     <FormProvider {...methods}>
-      <Container>
-        <section className='grid grid-cols-[1fr_23.125rem] gap-x-12.5 gap-y-10 pt-21 pb-10'>
-          <Header className='col-span-2' />
-          <CreateThreadPanel
-            editorModeParam={EDITOR_MODE_PARAM}
-            previewMode={PREVIEW_MODE}
-            formDefaultValues={defaultValues}
-          />
-          <Hints
-            editorModeParam={EDITOR_MODE_PARAM}
-            previewMode={PREVIEW_MODE}
-          />
-        </section>
-      </Container>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Container>
+          <section className='grid grid-cols-[1fr_23.125rem] gap-x-12.5 gap-y-10 pt-21 pb-10'>
+            <Header className='col-span-2' />
+            <CreateThreadPanel
+              editorModeParam={EDITOR_MODE_PARAM}
+              previewMode={PREVIEW_MODE}
+              formDefaultValues={defaultValues}
+            />
+            <Hints
+              editorModeParam={EDITOR_MODE_PARAM}
+              previewMode={PREVIEW_MODE}
+            />
+          </section>
+        </Container>
+      </Suspense>
     </FormProvider>
   );
 }
